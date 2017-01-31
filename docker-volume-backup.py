3bb3519c3d6ef53b0736df2686e327d29cb7e0bf
Roberto Salgado <drober@gmail.com>
2017-01-31 21:22:00 +0100

    List volumes properly

--- docker-volume-backup.py
+++ docker-volume-backup.py
@@ -17,7 +17,8 @@ for container in containers:
     backup_folders[container_name]['id'] = container.attrs['Id']
     backup_folders[container_name]['mounts'] = dict()
     backup_folders[container_name]['mounts']['folders'] = list()
-    
+    backup_folders[container_name]['status'] = container.attrs['State']['Status']
+
     for mount in container.attrs['Mounts']:
         backup_folders[container_name]['mounts']['folders'] += [{mount['Source']: mount['Destination']}]
 
@@ -25,20 +26,27 @@ for container in containers:
 # print(backup_folders)
 
 for container_name in backup_folders:
-#    print(container_folders['mounts'])
+    # print(container_folders['mounts'])
     container_id = backup_folders[container_name]['id']
-    if len(backup_folders[container_name]['mounts']['folders']) > 0:
-        logging.info("[{}] Container: {}".format(container_id,container_name))
+    container_status = backup_folders[container_name]['status']
+    container_volume_count = len(backup_folders[container_name]['mounts']['folders'])
+    if container_volume_count > 0:
+        logging.info("[{}] Container '{}' (Status: {}): {} volume(s) found".format(
+            container_id,
+            container_name,
+            container_status,
+            container_volume_count
+            )
+        )
         for folders in backup_folders[container_name]['mounts']['folders']:
             logging.info("[{}]\tHas folders: {}".format(container_id, folders))
     else:
-        logging.info("[{}] '{}' No volumes found".format(container_id, container_name))
+        logging.info("[{}] Container '{}' (Status: {}): No volumes found".format(
+            container_id,
+            container_name,
+            container_status
+            )
+        )
 
-#    for mounts in container_folders['mounts'].keys():
-#        for supreme_index in mounts:
-#            print(container_folders['mounts'][supreme_index])
-
-#    print("[{}] Backing up volume {}".format(folder['vol'], backup_folders[folder]))
-
-# containers = client.containers.run("ubuntu", "echo hello world", remove=True)
+# return_run = client.containers.run("ubuntu", "echo hello world", remove=True)
 
